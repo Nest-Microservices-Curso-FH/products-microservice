8a00b835d64ca34db454ff9fc20635a25202c789
tomasrpita <tomasrpita@gmail.com>
2024-05-09 06:32:17 +0200

    feat: Add pagination support to product listing

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
 
 @Controller('products')
 export class ProductsController {
@@ -19,9 +21,10 @@ export class ProductsController {
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
-
+  
   @Get()
-  findAll() {
+  findAll(@Query() paginationDto: PaginationDto) {
+    return paginationDto;
     return this.productsService.findAll();
   }
 
