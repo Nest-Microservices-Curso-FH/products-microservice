85caa599db724e1ddf9d2e58f4e328866c536d86
tomasrpita <tomasrpita@gmail.com>
2024-05-10 05:39:02 +0200

    chore: Add field "available" to schema and add index, change services accordingly

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -27,13 +27,16 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
 
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({ where: { available: true } });
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: {
+          available: true,
+        },
       }),
       meta: {
         total: totalPages,
@@ -44,7 +47,9 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   async findOne(id: number) {
-    const product = await this.product.findFirst({ where: { id } });
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
 
     if (!product) {
       throw new NotFoundException(`Product with id ${id} not found`);
@@ -65,8 +70,13 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async remove(id: number) {
     await this.findOne(id);
 
-    return this.product.delete({
+    // return this.product.delete({
+    //   where: { id },
+    // });
+
+    return this.product.update({
       where: { id },
+      data: { available: false },
     });
   }
 }
